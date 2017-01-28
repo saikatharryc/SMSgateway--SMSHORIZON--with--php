464ceb727c11ab208fe19142432cdc9dfce47904
Saikat Chakrabortty <saikatchakrabortty2@gmail.com>
2017-01-28 11:27:06 +0530

    database added

--- config.php
+++ config.php
@@ -3,6 +3,7 @@ Author -> Saiikat Chakrabortty.
 website-> Www.Saikat.cf
  -->
 
+
 <?php
 
 $numbr = $_POST['contactMobile'];
@@ -40,7 +41,62 @@ $ch = curl_init("http://smshorizon.co.in/api/sendsms.php?user=".$user."&apikey="
     curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
     $output = curl_exec($ch);      
      curl_close($ch); 
- echo $output;
+ //echo $output;
 
+/**********************
+ |Database MySql      |
+ | Connections and ops|
+ **********************/
+//connection
+ error_reporting( E_ALL & ~E_DEPRECATED & ~E_NOTICE );
+// turn on output buffering 
+ob_start();
+
+define('DB_DRIVER', 'mysql');
+define("DB_HOST", "localhost");
+define("DB_USER", "root");
+define("DB_PASSWORD", "");
+define("DB_DATABASE", "smsgateway");
+
+// basic options for PDO 
+$dboptions = array(
+    PDO::ATTR_PERSISTENT => FALSE,
+    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+    PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+    PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8',
+);
+
+//connect with the server
+try {
+    $DB = new PDO(DB_DRIVER . ':host=' . DB_HOST . ';dbname=' . DB_DATABASE, DB_USER, DB_PASSWORD, $dboptions);
+} catch (Exception $ex) {
+    echo($ex->getMessage());
+    die;
+}
+
+
+//operations
+$sql = "INSERT INTO `smstest` (name,mobile,msg,msg_id) 
+                              VALUES ('$name','$numbr','$message','$output')";  //add '$output' on the basics ,msg_id.
+try {
+        $stmt = $DB->prepare($sql);
+        $stmt->execute();
+        $results = $stmt->fetchAll();
+    } catch (Exception $ex) {
+       // echo($ex->getMessage());
+    }
+
+
+
+
+// CREATE TABLE `smstest` (
+//   `id` int(11) NOT NULL,
+//   `name` varchar(255) NOT NULL,
+//     `mobile` varchar(255) NOT NULL,
+//     `msg` text(255) NOT NULL,
+//     `msg_id` varchar(255) NOT NULL
+//   ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+// ALTER TABLE `smstest`
+//   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=20;
 
 ?>
